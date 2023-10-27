d169941d4fc83457d13dfeb0c73370378c32b5f7
IsidroHidalgo <Isidro.Hidalgo@uclm.es>
2023-10-27 07:19:31 +0200

    Espacio blanco intercolumna. Pies de figuras

--- Gráfico.R
+++ Gráfico.R
@@ -2,23 +2,32 @@ municipios$riesgo_clm_sPCA_EN <- factor(municipios$riesgo_clm_sPCA,
                                         labels = c("No risk", "Low risk", "Medium risk",
                                         "High risk"))
 png(filename = "sDRI.png", width = 9216, height = 9216, units = "px", res = 1200,
-    bg = )
+    bg = "#DDEDED")
 ggplot(data = municipios) +
   geom_sf(aes(fill = riesgo_clm_sPCA_EN), colour = "grey") +
   scale_fill_discrete(type = colores_riesgo_azul) +
-  ggtitle(label = "Depopulation risk in Castilla-La Mancha (sDRI indicator)") +
   theme(legend.position="bottom") +
-  easy_add_legend_title("Risk: ")
+  theme(plot.background = element_rect(fill = "#DDEDED")) +
+  easy_add_legend_title("Depopulation Risk: ")
 dev.off()
+
+tasa_crec <- cut(datos$tasa_crec_dem,
+                 breaks = c(-100, -20, -10, -5, 0, 5, 20, 2500),
+                 labels = c("loss >20%", "loss 10-20%",
+                            "loss 5-10%",
+                            "loss <5%", "gain <5%",
+                            "gain 5-20%", "gain >20%"))
 
 png(filename = "tasa_crec_dem.png", width = 9000, height = 7000, units = "px", res = 1200)
 ggplot(data = municipios) +
-  geom_sf(aes(fill = datos$riesgo_CLM_EN), colour = "grey") +
-  scale_fill_discrete(type = gama_JCCM) +
-  easy_add_legend_title("Type of zone: ")
+  geom_sf(aes(fill = tasa_crec), color = gris_muni) +
+  scale_fill_discrete(type = gama_JCCM_inv) +
+  geom_sf(data = nucleos, col = gris_muni, size = .001) +
+  easy_add_legend_title("Growth Rate: ")
 dev.off()
 
 
+# Anteriores
 grafA <- ggplot(data = municipios) +
   geom_sf(aes(fill = riesgo_clm_sPCA_EN), colour = "grey") +
   scale_fill_discrete(type = colores_riesgo_azul) +
